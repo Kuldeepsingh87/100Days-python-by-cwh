efac9dda77e0bd80b10a6f5abc2c0c588491c351
Kuldeep Singh <kuldepsingh864@gmail.com>
2026-04-24 22:25:43 +0530

    Added Day 13 file

---
+++ day13.py
@@ -0,0 +1,84 @@
+#upper case
+a = "kuldeep"
+print(a.upper())
+
+#lower case 
+b = "KULDEEP"
+print(b.lower())
+
+#rstrip case
+c = "kuldeep !!!!!"
+print(c.rstrip("!"))
+
+#replace case
+d = "BEEBU"
+print(d.replace("BEEBU","KULDEEP"))
+
+#split case
+e = "kuldeep singh nayak"
+print(e.split("k"))
+
+#capitalize case
+blogheading = "introduction to python"
+print(blogheading.capitalize())
+
+#center case
+f = "good morning everyone"
+print(len(f))
+print(len(f.center(50)))
+
+#count case
+g = "kuldeep singh nayak"
+print(g.count("k"))
+
+#endswith case
+h = "kuldeep !!!"
+print(h.endswith("!!!")) 
+
+#find case
+i = "hello my name is kunal"
+print(i.find("is"))
+
+#index case
+j = "hello i am from almora uttrakhand"
+print(j.index("am"))
+
+#isalnum case
+k = "welcometotheuttrakhand"
+print(k.isalnum())
+
+#isalpha case
+l = "welcometotheuttrakhand"
+print(l.isalpha())
+
+#islower case
+m = "control statement"
+print(m.islower())
+
+#isprintable case
+n = "we are all indians\n"
+print(n.isprintable())
+
+#isspace case
+o = "      "
+print(o.isspace())
+
+#istitle case
+p = "HELLO"
+print(p.istitle())
+
+#isupper case
+q = "don't waste your time"
+print(q.isupper())
+
+#startswith case
+r = "iit's running fast"
+print(r.startswith("iit's"))
+
+#swapcase
+s = "institute of technology gopeshwar"
+print(s.swapcase())
+
+#title case
+t = "ama boat ahead"
+print(t.title())

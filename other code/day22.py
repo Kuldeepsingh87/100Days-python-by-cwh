df5bda3a9cfacb51cc0af27f1a2459034c21b1fd
Kuldeep Singh <kuldepsingh864@gmail.com>
2026-05-08 00:12:00 +0530

    Added Python Day 22

---
+++ other code/day22.py
@@ -0,0 +1,19 @@
+#python lists
+marks = [2,3,4]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+print(marks[2])
+
+#list index
+colors = ["red","green","blue","silver"]
+print(colors[0])
+print(colors[1])
+print(colors[2])
+
+#negative indexing
+marks = [1,2,3,4]
+print(marks[-3])
+print(marks[len(marks)-3])
+
